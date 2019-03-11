1282ee826b1af4b91db882958859db214fb30ae6
Rizal Hermawan <rizalhrm24@gmail.com>
2019-03-11 18:05:31 +0700

    update reducers pokemon

--- src/public/redux/reducers/pokemon.js
+++ src/public/redux/reducers/pokemon.js
@@ -16,20 +16,37 @@ const initialState = {
       case 'ALL_POKEMONS_REJECTED':
         return { ...state, isLoading: false, isError: true }
       case 'ALL_POKEMONS_FULFILLED':
-        return { ...state, isLoading: false, pokemons: action.payload.data, length: action.payload.data.length}
-  
+        if(state.pokemons == ""){
+          state.pokemons = action.payload.data
+        }else{
+            if(action.payload.data.page == 1){
+                state.pokemons = action.payload.data
+            }else {
+                state.pokemons.page = action.payload.data.page;
+                action.payload.data.data.map(newData => {
+                    state.pokemons.data.push(newData);
+                })
+            }
+        }
+
+        return{
+            isLoading: false,
+            pokemons: state.pokemons,
+            length: action.payload.data.length
+        }
+
       case 'CREATE_POKEMON_PENDING':
         return {...state, isLoading: true, isSuccess: false, isError: false};
       case 'CREATE_POKEMON_FULFILLED':
-        state.pokemons.push(action.payload.data);
-        return {...state, pokemons: state.pokemons, isLoading: false, isSuccess: true};
+        state.pokemons.data.push(action.payload.data);
+        return {...state, pokemons: state.pokemons.data, isLoading: false, isSuccess: true};
       case 'CREATE_POKEMON_REJECTED':
         return {...state, isLoading: false, isError: true};
   
       case 'UPDATE_POKEMON_PENDING':
         return {...state, isLoading: true, isSuccess: false, isError: false};
       case 'UPDATE_POKEMON_FULFILLED':
-        const newPokemonsAfterUpdate = state.pokemons.map(pokemon => {
+        const newPokemonsAfterUpdate = state.pokemons.data.map(pokemon => {
           if (pokemon.id == action.payload.data.id) {
             return action.payload.data;
           }
